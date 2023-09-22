1bd9b40f7f3f3147861240366a85a64a7d303c1f
sadik <sadik@ldd.ca>
2023-09-22 14:18:36 -0400

    selenium version change

--- src/test/java/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features="src/test/java/features",glue="project.stepDefinations",
-monochrome=true,tags= "@Regression" ,plugin= {"html:reports/cucumber.html"})//tags= "@Regression", "@Regression2",
+monochrome=true,tags= "@Regression2" ,plugin= {"html:reports/cucumber.html"})//tags= "@Regression", "@Regression2",
 public class TestNGTestRunner extends AbstractTestNGCucumberTests{
 	
 }
